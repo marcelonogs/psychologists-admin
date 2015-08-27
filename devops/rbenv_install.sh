abe881cabcb54bbcb150d1be5c97b8e28db5cd01
David Dieulivol <david.dieulivol@gmail.com>
2015-08-27 21:24:21 +0100

    [Docs] Updates the getting started of the readme.

--- devops/rbenv_install.sh
+++ devops/rbenv_install.sh
@@ -5,9 +5,3 @@ echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 
 echo Installing Ruby Build
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-
-# Note: Before doing this one, you need to remove the first lines of the .bashrc about non-interactive shells.
-echo Installing Ruby 2.1.2
-source /home/vagrant/.bashrc
-rbenv install 2.1.2
-rbenv global 2.1.2
